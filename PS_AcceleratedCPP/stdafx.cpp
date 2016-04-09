14f7cd938e843a8b7bfe018e2ed2cbdfe20c2fda
pcallisto <pietro.callisto@gmail.com>
2016-04-09 14:34:00 -0400

    Add module

---
+++ PS_AcceleratedCPP/stdafx.cpp
@@ -0,0 +1,10 @@
+// stdafx.cpp : source file that includes just the standard includes
+// PS_AcceleratedCPP.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+#include "UsrLib.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
